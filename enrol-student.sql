eceeea1c86378b94eb31639a478a2b432c171e6b
Diego <67525455+champi-r@users.noreply.github.com>
2022-03-10 22:47:49 -0300

    Estudiantes Matriculados Lite
    Se crea archivo

---
+++ enrol-student.sql
@@ -0,0 +1,9 @@
+SELECT co.id, co.fullname as Curso, u.id, u.firstname, u.lastname, u.email
+FROM prefix_role_assignments ra
+JOIN prefix_context con ON con.id=ra.contextid
+JOIN prefix_course AS co ON co.id=con.instanceid
+JOIN prefix_user AS u ON u.id=ra.userid
+WHERE
+con.contextlevel=50
+AND ra.roleid=5
+ORDER BY co.id
