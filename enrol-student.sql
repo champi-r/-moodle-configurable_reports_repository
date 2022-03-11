a96ebce2b30d0818d82b2011332251f6b461f843
Diego <67525455+champi-r@users.noreply.github.com>
2022-03-10 22:53:23 -0300

    modificacion en select
    se agrega alias al campo co.id y se agrega campo u.username

--- enrol-student.sql
+++ enrol-student.sql
@@ -1,4 +1,4 @@
-SELECT co.id, co.fullname as Curso, u.id, u.firstname, u.lastname, u.email
+SELECT co.id as courseid, co.fullname as Curso, u.id, u.username, u.firstname, u.lastname, u.email
 FROM prefix_role_assignments ra
 JOIN prefix_context con ON con.id=ra.contextid
 JOIN prefix_course AS co ON co.id=con.instanceid
